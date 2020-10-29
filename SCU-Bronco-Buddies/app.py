327f3410ac9116e71aa7eab72d04dd5c4d71de8e
Chelsea <ccfernandes@scu.edu>
2020-10-28 18:11:07 -0700

    integrated nav bar with templates

--- SCU-Bronco-Buddies/app.py
+++ SCU-Bronco-Buddies/app.py
@@ -11,19 +11,19 @@ def home():
     return render_template('home.html')
 
 
-# # login page  
-# @app.route("/login", methods=['GET', 'POST'])
-# def login():
-#     form=LoginForm()
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(email=form.email.data).first()
-#         if user and bcrypt.check_password_hash(user.password, form.password.data):
-#             login_user(user)
-#             next_page = request.args.get('next') # redirect to next page if it exits (user tried accessing and is now logging in)
-#             return redirect(next_page) if next_page else redirect(url_for('profile'))
-#         else:
-#             flash('Login unsucessful. Please check credentials.', 'danger')
-#     return render_template('templates.html', title='Login', form=form)
+# login page  
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    # form=LoginForm()
+    # if form.validate_on_submit():
+    #     user = User.query.filter_by(email=form.email.data).first()
+    #     if user and bcrypt.check_password_hash(user.password, form.password.data):
+    #         login_user(user)
+    #         next_page = request.args.get('next') # redirect to next page if it exits (user tried accessing and is now logging in)
+    #         return redirect(next_page) if next_page else redirect(url_for('profile'))
+    #     else:
+    #         flash('Login unsucessful. Please check credentials.', 'danger')
+    return render_template('login.html', title='Login')
 
 
 # register page 
@@ -31,13 +31,14 @@ def home():
 def register():
     # if current_user.is_authenticated:
     #     return redirect(url_for('user'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(firstname=escape(form.firstname.data), lastname=escape(form.lastname.data), email=escape(form.email.data),
-                    password=escape(hashed_password))
-        db.session.add(user)
-        db.session.commit()
-        flash('Your account has been created. You are now able to log in!', 'success')
-        return redirect(url_for('login'))
-    return render_template('templates.html', title='Register', form=form)
+    
+    # form = RegistrationForm()
+    # if form.validate_on_submit():
+    #     hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+    #     user = User(firstname=escape(form.firstname.data), lastname=escape(form.lastname.data), email=escape(form.email.data),
+    #                 password=escape(hashed_password))
+    #     db.session.add(user)
+    #     db.session.commit()
+    #     flash('Your account has been created. You are now able to log in!', 'success')
+    #     return redirect(url_for('login'))
+    return render_template('signup.html', title='Register')
--- SCU-Bronco-Buddies/models.py
+++ SCU-Bronco-Buddies/models.py
@@ -12,10 +12,9 @@ def init_db():
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(40), nullable=False)
-    lastname = db.Column(db.String(40))
+    username = db.Column(db.String(40), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
 
     def __repr__(self):
-        return f"User('{self.firstname}', '{self.lastname}', '{self.email}')"
+        return f"User('{self.username}', '{self.email}')"
